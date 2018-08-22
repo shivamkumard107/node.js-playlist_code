d6eee28c4e16bca8edec554757085e6acdc3d7c3
shivamkumard107 <shivam.kumard107@gmail.com>
2018-08-22 14:13:55 +0530

    finnished 6th tuts. learning basics

---
+++ app.js
@@ -0,0 +1,12 @@
+var counter = require('./count');
+console.log(counter(['shawn', 'shivam', 'mohit']));
+
+// //normal function statement
+// function sayHi(){
+//   console.log('Hiii!!');
+// }
+//
+// sayHi();
+// //function expression
+//
+// var sayBye = function(){};
